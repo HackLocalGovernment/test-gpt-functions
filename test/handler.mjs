22c19e014da1cb3fbedcaf4d1681c3b66f5487a6
Craig R. Hughes <craig.git@rungie.com>
2023-11-15 11:26:15 -0800

    Slightly better test

--- test/handler.mjs
+++ test/handler.mjs
@@ -9,6 +9,6 @@ describe('handleJSON', () => {
             date: '2021-08-25',
         };
         const result = handleJSON(json);
-        expect(result).toEqual(expect.any(String));
+        expect(result).toStrictEqual(expect.stringContaining('We are looking for the agenda and minutes of the Town Council meeting on 8/25/2021 which is probably linked from https://www.portolavalley.net/community/town-calendar/-curm-8/-cury-2021.'));
     });
 });
